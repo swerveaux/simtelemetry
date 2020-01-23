881e2fa3fcae9117eb2966c7b4465bb292a7c272
Brian Nilsen <bnilsen@ziprecruiter.com>
2020-01-23 10:58:14 -0800

    Allow port numbers for servers to be set from the command line

--- cmd/simtelemetry/main.go
+++ cmd/simtelemetry/main.go
@@ -8,18 +8,27 @@ import (
 	"github.com/swerveaux/simtelemetry/internal/webserver"
 
 	"github.com/swerveaux/simtelemetry/internal/server"
+	flag "github.com/spf13/pflag"
 )
 
-const port = 4843
+type Config struct {
+	UDPPort int
+	HTTPPort int
+}
 
 func main() {
-	fmt.Println("Starting server")
+	var c Config
+
+	flag.IntVar(&c.HTTPPort, "http-port", 10001, "Port to run http server on for displaying telemetry")
+	flag.IntVar(&c.UDPPort, "udp-port", 4843, "Port to run UDP listener on.")
+	flag.Parse()
+	fmt.Printf("Starting UDP server on %d, HTTP server on %d\n", c.UDPPort, c.HTTPPort)
 	ch := make(chan []byte)
 	var wg sync.WaitGroup
 
 	wg.Add(2)
 	go func() {
-		err := server.Run(port, ch)
+		err := server.Run(c.UDPPort, ch)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
@@ -28,7 +37,7 @@ func main() {
 	}()
 
 	go func() {
-		err := webserver.Run(10001, ch)
+		err := webserver.Run(c.HTTPPort, ch)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
